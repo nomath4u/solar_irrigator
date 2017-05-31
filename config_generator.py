0b0f3cdf7e42691c38e7fe92c14187c0a768b816
Chris Harper <nomath4u@gmail.com>
2017-05-30 22:22:13 -0700

    Added ability to send the temperature back to a private mqtt server. Updated the config script to take this into account and also to add quotes where needed for defined strings

--- config_generator.py
+++ config_generator.py
@@ -2,9 +2,17 @@
 
 #print("test")
 
-name = input("Input username: ")
+name = input("Input Adafruit username: ")
 key = input("Input adafruit.io key: ")
+home_ip = input("Input private MQTT Broker IP: ")
+home_port = input("Input private MQTT Broker port: ")
+home_user = input("Input private MQTT Broker user: ")
+home_pass = input("Input priivate MQTT Broker password: ")
 
 f = open("./irrigation.h", 'w')
-print("#define AIO_USERNAME " + name,file=f)
-print("#define AIO_KEY " + key,file=f)
+print("#define AIO_USERNAME \"" + name +"\"",file=f)
+print("#define AIO_KEY \"" + key +"\"",file=f)
+print("#define HOME_SERVER \"" +home_ip +"\"",file=f)
+print("#define HOME_SERVERPORT " +home_port ,file=f)
+print("#define HOME_USERNAME \"" +home_user +"\"",file=f)
+print("#define HOME_PASSWORD \"" +home_pass +"\"",file=f)
--- irrigation.ino
+++ irrigation.ino
@@ -13,14 +13,17 @@
 #define CLK_PIN         D0
 /************ Global State (you don't need to change this!) ***   ***************/ 
 TCPClient TheClient; 
+TCPClient HomeClient;
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details. 
 Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
+Adafruit_MQTT_SPARK home_mqtt(&HomeClient,HOME_SERVER,HOME_SERVERPORT,HOME_USERNAME,HOME_PASSWORD); 
 /****************************** Feeds ***************************************/ 
 // Setup a feed called 'voltage' for publishing. 
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname> 
 Adafruit_MQTT_Publish moisture = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Moisture Log"); 
 Adafruit_MQTT_Publish temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Irrigation Temp"); 
 Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/irrigation_hummidity"); 
+Adafruit_MQTT_Publish home_temp = Adafruit_MQTT_Publish(&home_mqtt, "home/outside_temp"); 
 // Setup a feed called 'onoff' for subscribing to changes. 
 //Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff"); 
 /*************************** Sketch Code ************************************/ 
@@ -47,6 +50,9 @@ void loop()
        humidity.publish((int)sht1x.readHumidity());
        Serial.println(sht1x.readTemperatureC());
  } 
+ if( home_mqtt.Update() ){
+       home_temp.publish((int)sht1x.readTemperatureC());
+ }
    //delay(2000);
    System.sleep(SLEEP_MODE_DEEP, 60); //Value is in seconds. With thins you need to be able to pull wake up pin high to re-program it.
 } 
